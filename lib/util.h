2d6d0163569b939b6acac7a04705d13e942f0ee3
Aqil Contractor <aqilcm@gmail.com>
2024-01-31 02:12:13 -0500

    Added a define for letting me make programs in any language and just have them as a string while maybe getting highlighted.

--- lib/util.h
+++ lib/util.h
@@ -1,5 +1,5 @@
-
 #include <stdint.h>
+
 typedef int8_t  i8;
 typedef int16_t i16;
 typedef int32_t i32;
@@ -9,3 +9,6 @@ typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+
+
+#define PROG(...) #__VA_ARGS__
