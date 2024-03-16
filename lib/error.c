6a5bf6c530d773e5fbfde5e7bdb22973ca0bd4d0
Aqil Contractor <aqilcm@gmail.com>
2024-03-16 01:54:00 -0400

    [Errors] Improve every method.

--- lib/error.c
+++ lib/error.c
@@ -3,34 +3,57 @@
 #include "util.h"
 
 // Every single piece of info about a line you'd ever want
-static inline u32 line(char* str, u32* place, char** start, char** end) {
-	
-	// Calculates end of line
-	char* e = str + *place;
-	while(e && *e != '\n') e ++;
-	*end = e;
+static inline u32 line(char* str, u32 place, u32* start, u32* end) {
 
-	// Calculates start of line
-	e = str + *place;
-	while(e > str && *e != '\n') e--;
-	*start = e;
-	*place -= (e - str); // Normalizes `place` to the current line
-
-	// Calculates line_count
-	u32 line_count = 1;
-	while (e > str) {
-		if(*e == '\n') line_count++;
-		e --;
+	if(end) {
+		u32 cur = place;
+		while(str[cur] && str[cur] != '\n') cur ++;
+		*end = cur;
 	}
+
+	u32 cur = place;
+	while(cur > 0 && str[cur] != '\n') cur --;
+	if(start) *start = cur;
+	
+	u32 line_count = 1;
+	for(; cur > 0; cur --) if(str[cur] == '\n') line_count ++;
 	return line_count;
 }
 
 // Counts the length of an integer
-static inline u32 intlen(int i) {
-	u32 j = 0;
-	if(i < 0) i = -i, j++;
-	while ((i /= 10) > 10) j++;
-	return j;
+static inline int count_digits(uint32_t n) {
+#ifndef __has_builtin
+#  define __has_builtin(x) 0
+#endif
+#if __has_builtin(__builtin_clz)
+  // This increments the upper 32 bits (log10(T) - 1) when >= T is added.
+  #  define K(T) (((sizeof(#T) - 1ull) << 32) - T)
+  static const uint64_t table[] = {
+      K(0),          K(0),          K(0),           // 8
+      K(10),         K(10),         K(10),          // 64
+      K(100),        K(100),        K(100),         // 512
+      K(1000),       K(1000),       K(1000),        // 4096
+      K(10000),      K(10000),      K(10000),       // 32k
+      K(100000),     K(100000),     K(100000),      // 256k
+      K(1000000),    K(1000000),    K(1000000),     // 2048k
+      K(10000000),   K(10000000),   K(10000000),    // 16M
+      K(100000000),  K(100000000),  K(100000000),   // 128M
+      K(1000000000), K(1000000000), K(1000000000),  // 1024M
+      K(1000000000), K(1000000000)                  // 4B
+  };
+  return (n + table[__builtin_clz(n | 1) ^ 31]) >> 32u;
+#else
+  int count = 1;
+  for (;;) {
+    if (n < 10) return count;
+    if (n < 100) return count + 1;
+    if (n < 1000) return count + 2;
+    if (n < 10000) return count + 3;
+    n /= 10000u;
+    count += 4;
+  }
+  return count;
+#endif
 }
 
 #define ERRORSTRMAXWIDTH 200
@@ -38,28 +61,27 @@ static inline u32 intlen(int i) {
 /*
 	Makes an error like
 ----------------------------------
-L18@8: Unexpected identifier
+test.c@18:8: Unexpected identifier
 18 | let br;h
    |        ^
 ----------------------------------
 */
-void error_at(char* str, u32 place, char* fmt, ...) {
-	char* start,* end;
-	u32 linenum = line(str, &place, &start, &end);
-	u32 linenumlen = intlen(linenum);
+void parser_message(char* str, u32 place, char* file, char* fmt, ...) {
+	u32 start, end;
+	u32 linenum = line(str, place, &start, &end);
+	u32 linenumlen = count_digits(linenum);
 	// u32 len = start - end - 1 + 3 + linenumlen;
 
 	// The error
 	va_list args;
 	va_start(args, fmt);
-	fprintf(stderr, "L%d@%d:", linenum, place);
+	fprintf(stderr, "%s@%d:%d:", file, linenum, place - start);
 	vfprintf(stderr, fmt, args);
 	fprintf(stderr, "\n");
 
 	// Prints out the line
-	fprintf(stderr, "%d | %.*s\n", linenum, (u32) (start - end - 1), start);
+	fprintf(stderr, "%d | %.*s\n", linenum, (u32) (start - end - 1), str + start);
 
 	// Prints out the spaces and then the ^
-	fprintf(stderr, "%*s|%*s\\033[31;1m^\\033[0m\n", linenumlen + 1, "", place - linenumlen - 2, "");
+	fprintf(stderr, "%*s|%*s\\033[31;1m^\\033[0m\n", linenumlen + 1, "", place - linenumlen - 2 - start, "");
 }
-
--- lib/error.h
+++ lib/error.h
@@ -1,2 +1,11 @@
 #pragma once
-void error_at(char* str, u32 place, char* fmt, ...);
+#include "util.h"
+
+// void error_at(char* str, u32 place, char* fmt, ...);
+
+#if defined __clang__ || defined __GNUC__
+__attribute__((format(printf, 4, 5)))
+#endif
+void parser_message(char* str, u32 place, char* file, char* fmt, ...);
+
+#define error_at(str, place, file, ...) parser_message(str, place, file, "error: " __VA_ARGS__)
