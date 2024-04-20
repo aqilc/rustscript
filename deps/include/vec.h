75f607c0f71560582c38b38396526f024a1a7eb4
Aqil Contractor <aqilcm@gmail.com>
2024-04-20 16:20:26 -0400

    [vec.h] Add VEC_H_MORE_MACROS.

--- deps/include/vec.h
+++ deps/include/vec.h
@@ -10,6 +10,8 @@
  * Options provided by defines before including:
  *   VEC_H_STATIC_INLINE:
  *     Implements all methods as static inline, for increased performance when calling. Automatically defines VEC_H_IMPLEMENTATION.
+ *   VEC_H_MORE_MACROS:
+ *     Instead of defining functions for extremely simple operations, exports them as macros instead
  *   VEC_H_CALLOC:
  *     Name for user-provided calloc(size_t, size_t) function.
  *   VEC_H_REALLOC:
@@ -73,11 +75,20 @@ struct vecdata_ {
 #define vunshift(x, ...) (*(typeof(x))vunshift_((void**)&(x), sizeof(*(x))) = (typeof(*x)) __VA_ARGS__)
 
 // Simplifies vclear and vtostr calls
-#define vtostr(v) vtostr_((void**)&(v))
+// #define vtostr(v) vtostr_((void**)&(v))
 
 // Pops off the last element and returns it
-#define vpop(x) (_DATA(x)->data + (_DATA(x)->used -= sizeof(*(x))))
-// #define vpop(x) vpop_((x), sizeof(*(x)))
+#ifdef VEC_H_MORE_MACROS
+	#define vpop(x) (_DATA(x)->data + (_DATA(x)->used -= sizeof(*(x))))
+	#define vpopn(x, n) (_DATA(x)->data + (_DATA(x)->used -= (n) * sizeof(*(x)))
+	#define vpopto(x, idx) (_DATA(x)->data + (_DATA(x)->used = (idx) * sizeof(*(x)))
+	#define vempty(x) (_DATA(x)->data + (_DATA(x)->used = 0))
+	#define vfree(x) VEC_H_FREE(_DATA(x))
+#else
+	#define vpop(x) vpop_((x), sizeof(*(x)))
+	#define vpopn(x, n) vpop_((x), (n) * sizeof(*(x)))
+	#define vpopto(x, n) vpop_((x), (vlen(x) - n) * sizeof(*(x)))
+#endif
 
 // Removes data from the middle of the array
 #define vremove(x, idx) vremove_((x), sizeof(*(x)), (idx))
@@ -91,6 +102,9 @@ struct vecdata_ {
 // New vector initialized with a struct or array
 #define vecify(x) ((typeof(x))vnewn(sizeof(x)) = x)
 
+// For loop that iterates over the vector
+#define vfor(x, y) for(typeof(x) y = x, _end = x + vlen(x); y < _end; y ++)
+
 // V String that has length info and is automatically push-able
 typedef char* vstr;
 
@@ -101,23 +115,25 @@ VEC_H_EXTERN void* vnewn(uint32_t n);
 
 // Returns a *new* concatenated vector, use `pushv` if you don't want a new vec :D
 VEC_H_EXTERN void* vcat(void* a, void* b);
-VEC_H_EXTERN void vfree(void* v);
 
 // Returns a 1 if they're different, 0 if they're the same
-VEC_H_EXTERN char vcmp(void* a, void* b);
+VEC_H_EXTERN char  vcmp(void* a, void* b);
 
 // Initialize a vector with a string straight away
 // VEC_H_EXTERN char* strtov(char* s);
-VEC_H_EXTERN void  vempty(void* v);
-VEC_H_EXTERN char* vtostr_(void** v);
+VEC_H_EXTERN char* vtostr(void* v);
 VEC_H_EXTERN void  vremove_(void* v, uint32_t size, uint32_t pos);
 VEC_H_EXTERN void* vpush_(void** v, uint32_t size);
 VEC_H_EXTERN void  vpushsf_(void** v, char* fmt, ...);
 VEC_H_EXTERN void  vpushn_(void** v, uint32_t n, uint32_t size, void* thing);
 // VEC_H_EXTERN void* vunshift_(void** v, uint32_t size);
-VEC_H_EXTERN void* vpop_(void* v, uint32_t size);
 VEC_H_EXTERN char* vfmt(char* str, ...);
 
+#ifndef VEC_H_MORE_MACROS
+	VEC_H_EXTERN void* vpop_(void* v, uint32_t size);
+	VEC_H_EXTERN void* vempty(void* v);
+	VEC_H_EXTERN void  vfree(void* v);
+#endif
 
 #endif
 
@@ -159,8 +175,8 @@ VEC_H_EXTERN char* vfmt(char* str, ...);
 
 // Combines two vectors into a new vector
 VEC_H_EXTERN void* vcat(void* a, void* b) {
-	struct vecdata_* v = VEC_H_CALLOC(1, ((struct vecdata_*) b)->used + ((struct vecdata_*) a)->used + sizeof(struct vecdata_));
-	v->cap = v->used = ((struct vecdata_*) b)->used + ((struct vecdata_*) a)->used;
+	struct vecdata_* v = VEC_H_CALLOC(1, _DATA(b)->used + _DATA(a)->used + sizeof(struct vecdata_));
+	v->cap = v->used = _DATA(b)->used + _DATA(a)->used;
 	v += 1;
 	memcpy(v, a, _DATA(a)->used);
 	memcpy((char*) v + _DATA(a)->used, b, _DATA(b)->used);
@@ -168,10 +184,10 @@ VEC_H_EXTERN void* vcat(void* a, void* b) {
 }
 
 VEC_H_EXTERN char vcmp(void* a, void* b) {
-	uint32_t len = ((struct vecdata_*) a)->used;
-	if(len != ((struct vecdata_*) b)->used) return 1;
-	uint32_t idx = 0;
-	while (idx < len) if(((char*)a)[idx] != ((char*)b)[idx]) return 1; else idx ++;
+	uint32_t len = _DATA(a)->used;
+	if(len != _DATA(b)->used) return 1;
+	for(uint32_t idx = 0; idx < len; idx ++)
+		if(((char*)a)[idx] != ((char*)b)[idx]) return 1;
 	return 0;
 }
 
@@ -184,14 +200,20 @@ VEC_H_EXTERN char* strtov(char* s) {
 	return (char*) v;
 }
 
-VEC_H_EXTERN char* vtostr_(void** v) {
-	(*(char*)vpush_(v, 1)) = 0;
-	_DATA(*v)->used --;
-	return *v;
+VEC_H_EXTERN char* vtostr(void* v) {
+	uint32_t len = _DATA(v)->used;
+	char* str = malloc(len + 1);
+	memcpy(str, v, len);
+	str[len] = 0;
+	return str;
 }
 
-VEC_H_EXTERN void vempty(void* v) { _DATA(v)->used = 0; }
-VEC_H_EXTERN void vfree(void* v) { VEC_H_FREE(_DATA(v)); }
+#ifndef VEC_H_MORE_MACROS
+	VEC_H_EXTERN void* vempty(void* v) { _DATA(v)->used = 0; return v; }
+	VEC_H_EXTERN void* vpop_(void* v, uint32_t size) { _DATA(v)->used -= size; return _DATA(v)->data + _DATA(v)->used; }
+	VEC_H_EXTERN void vfree(void* v) { VEC_H_FREE(_DATA(v)); }
+#endif
+
 
 
 // Reallocs more size for the array, hopefully without moves
@@ -243,8 +265,6 @@ VEC_H_EXTERN void vpushn_(void** v, uint32_t n, uint32_t size, void* thing) {
 	if(size == 1) memset(place, *((char*) thing), size);
 	else for(uint32_t i = 0; i < n; i ++) memcpy(place + size * i, thing, size);
 }
-
-// VEC_H_EXTERN void* vpop_(void* v, uint32_t size) { _DATA(v)->used -= size; return _DATA(v)->data + _DATA(v)->used; }
 
 // Adds an element at the start of the vector, ALSO CHANGES PTR
 VEC_H_EXTERN void* vunshift_(void** v, uint32_t size) {
