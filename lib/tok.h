e8c02e640000b372482db11e88ba55bb460542b5
Aqil Contractor <aqilcm@gmail.com>
2024-04-30 21:43:24 -0400

    [tok] Change SHIFT<direction> to just SH<L/R>

--- lib/tok.h
+++ lib/tok.h
@@ -9,12 +9,12 @@ enum RS_TokenType {
 
 	// Places that depend on operator order: parse.c:precedences
 
-	// THESE MUST BE KEPT IN ORDER, THE ASSIGNMENT OPERATIONS MUST BE 13 ABOVE THEIR NORMAL COUNTERPARTS.
+	// THESE MUST BE KEPT IN ORDER
 	TT_OPSET, TT_OPADD, TT_OPSUB, TT_OPMUL, TT_OPDIV, TT_OPMOD, TT_OPPOW, // basic operations
 	TT_OPBXOR, TT_OPBOR, TT_OPBAND, // bitwise operations
-	TT_OPBSHIFTRIGHT, TT_OPBSHIFTLEFT, // bitshifts
+	TT_OPBSHR, TT_OPBSHL, // bitshifts
 	TT_OPADDSET, TT_OPSUBSET, TT_OPMULSET, TT_OPDIVSET, TT_OPMODSET, TT_OPPOWSET,
-		TT_OPBXORSET, TT_OPBORSET, TT_OPBANDSET, TT_OPBSHIFTRIGHTSET, TT_OPBSHIFTLEFTSET, // operations with assignment
+		TT_OPBXORSET, TT_OPBORSET, TT_OPBANDSET, TT_OPBSHRSET, TT_OPBSHLSET, // operations with assignment
 
 	TT_OPBNOT,
 	TT_OPDOT, TT_OPQUESDOT, TT_OPQUES,
@@ -27,11 +27,12 @@ enum RS_TokenType {
 	
 	TT_LAND, TT_LOR, TT_LTERNARY, TT_LNOT, // Logic
 	TT_CGREATER, TT_CLESS, TT_CEQ, TT_CGREATEQ, TT_CLESSEQ, TT_CNOTEQ, // Comparisons
-	
+
+	// KRETURN **HAS** TO BE FIRST!
 	TT_KRETURN, TT_KLET, TT_KCONST, TT_KIF, TT_KELSE, TT_KFN, TT_KSTRUCT, TT_KTRAIT,
 
-	TT_TU8, TT_TU16, TT_TU32, TT_TU64, TT_TI8, TT_TI16, TT_TI32, TT_TI64,
-		TT_TBOOL,
+	TT_TU8, TT_TU16, TT_TU32, TT_TU64, TT_TI8, TT_TI16, TT_TI32, TT_TI64, TT_TF32, TT_TF64,
+		TT_TBOOL, TT_TSTRING, TT_TNUMBER, TT_TOBJECT,
 
 	TT_IDENT, TT_INT, TT_FLOAT, TT_STRING,
 
@@ -53,6 +54,10 @@ typedef struct RS_Token RS_Token;
 
 static inline const bool isop(RS_TokenType type) {
 	return type > TT_DNUOPSTART && type < TT_DNUOPEND;
+}
+
+static inline const bool iskeyword(RS_TokenType type) {
+	return type > TT_KRETURN && type < TT_KTRAIT;
 }
 
 RS_Token* tokenize(char* source);
