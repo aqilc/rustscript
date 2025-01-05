fb4b0c5fa9494829f953fe6ea3e3f79f86303560
Aqil Contractor <aqilcm@gmail.com>
2025-01-04 23:12:44 -0500

    Make test suite much more comprehensive.

--- tests/asm.c
+++ tests/asm.c
@@ -1,25 +1,27 @@
 // #define NO_PRINT
-#define INT BRUH_WHY_FUCK_YOU
+#define INT WINDOWS_INT
 #include "tests.h"
 #undef FAR
 #undef IN
 #undef INT
 #undef OUT
-#include <asm/asm_x86.h>
+#include <asm/asm_x64.h>
 
 int instructionlen;
 int expectedinslen;
 x64Ins curins;
-double hi;
 
-#define INSTEST(bytes, ...) curins = (x64Ins) { __VA_ARGS__ }; SUB(#__VA_ARGS__ " => " #bytes) {\
-	instructionlen = x64emit(&curins, buf);\
-	expectedinslen = sizeof(#bytes) / 5;\
-	expecteq(instructionlen, expectedinslen);
-#define INSTESTMEMEQ(...) assertbyteseq(buf, { __VA_ARGS__ }); } SUBBENCH() { x64emit(&curins, buf); }
+#define INSTEST(bytes, ...) \
+	curins = (x64Ins) { __VA_ARGS__ };\
+	SUB(#__VA_ARGS__ " => " #bytes) {\
+		instructionlen = x64emit(&curins, buf);\
+		expectedinslen = sizeof(#bytes) / 5;\
+		if(instructionlen == 0) puts(get_error(NULL));\
+		expecteq(instructionlen, expectedinslen);
+#define INSTESTMEMEQ(...) expectbyteseq(buf, { __VA_ARGS__ }); } SUBBENCH() { x64emit(&curins, buf); }
 
 TEST("Assemble push instructions (1 argument, with optional REX and Addressing Overrides)") {
-	char buf[3] = {0};
+	uint8_t buf[3] = {0};
 
 	benchiters(12345);
 
@@ -37,7 +39,7 @@ TEST("Assemble push instructions (1 argument, with optional REX and Addressing O
 }
 
 TEST("Assemble call/jmp instructions (1 argument, with optional REX and Immediates)") {
-	char buf[7] = {0};
+	uint8_t buf[7] = {0};
 
 	INSTEST(0xFF 0xD0, CALL, rax);
 	INSTESTMEMEQ(0xFF, 0xD0);
@@ -47,10 +49,13 @@ TEST("Assemble call/jmp instructions (1 argument, with optional REX and Immediat
 }
 
 TEST("Assemble mov instructions (2 arguments, with optional REX and Immediates)") {
-	char buf[10] = {0};
+	uint8_t buf[10] = {0};
 	
-	INSTEST(0xB0 0x03, MOV, al, im8(3));
+	INSTEST(0xB0 0x03, MOV, al, imm(3));
 	INSTESTMEMEQ(0xB0, 0x03);
+
+	INSTEST(0x67 0x8A 0x4B 0x03, MOV, cl, mem($ebx, 3));
+	INSTESTMEMEQ(0x67, 0x8A, 0x4B, 0x03);
 
 	INSTEST(0x48 0xC7 0xC0 0x04 0x00 0x00 0x00, MOV, rax, imm(4));
 	INSTESTMEMEQ(0x48, 0xC7, 0xC0, 0x04, 0x00, 0x00, 0x00);
@@ -58,15 +63,21 @@ TEST("Assemble mov instructions (2 arguments, with optional REX and Immediates)"
 	INSTEST(0x4C 0x89 0xC0, MOV, rax, r8);
 	INSTESTMEMEQ(0x4C, 0x89, 0xC0);
 
-	INSTEST(0x67 0x8A 0x4B 0x03, MOV, cl, mem($ebx, 3));
-	INSTESTMEMEQ(0x67, 0x8A, 0x4B, 0x03);
+	INSTEST(0x4D 0x89 0xEC, MOV, r12, r13);
+	INSTESTMEMEQ(0x4D, 0x89, 0xEC);
+
+	INSTEST(0x48 0xBA 0x39 0x20 0x39 0x20 0x09 0x02 0x00 0x00, MOV, rdx, imm(0x20920392039));
+	INSTESTMEMEQ(0x48, 0xBA, 0x39, 0x20, 0x39, 0x20, 0x09, 0x02, 0x00, 0x00);
+
+	INSTEST(0xF3 0x44 0x0F 0x10 0x3C 0x48, MOVSS, xmm15, mem($rax, 0, $rcx, 2));
+	INSTESTMEMEQ(0xF3, 0x44, 0x0F, 0x10, 0x3C, 0x48);
 
 	INSTEST(0x67 0x66 0x0F 0x28 0x40 0x0A, MOVAPD, xmm0, mem($eax, 0xA));
 	INSTESTMEMEQ(0x67, 0x66, 0x0f, 0x28, 0x40, 0x0A);
 }
 
 TEST("Assemble lea instructions (2 arguments, with optional REX and Immediates)") {
-	char buf[10] = {0};
+	uint8_t buf[10] = {0};
 
 	INSTEST(0x49 0x8D 0x49 0x08, LEA, rcx, mem($r9, 8));
 	INSTESTMEMEQ(0x49, 0x8D, 0x49, 0x08);
@@ -78,11 +89,14 @@ TEST("Assemble lea instructions (2 arguments, with optional REX and Immediates)"
 	INSTESTMEMEQ(0x48, 0x8d, 0x05, 0x08, 0x00, 0x00, 0x00);
 }
 
-TEST("Assemble FPU Instructions (1-2 arguments with different, FPU operand types)") {
-	char buf[10] = {0};
+TEST("Assemble Special Instructions (1-2 arguments with different, FPU operand types)") {
+	uint8_t buf[10] = {0};
 
 	INSTEST(0xD9 0xC0, FLD, st0);
 	INSTESTMEMEQ(0xD9, 0xC0);
+
+	INSTEST(0xD9 0xE8, FLD1);
+	INSTESTMEMEQ(0xD9, 0xE8);
 
 	INSTEST(0xDD 0x04 0x25 0x02 0x00 0x00 0x00, FLD, m64($none, 2));
 	INSTESTMEMEQ(0xDD, 0x04, 0x25, 0x02, 0x00, 0x00, 0x00);
@@ -90,15 +104,27 @@ TEST("Assemble FPU Instructions (1-2 arguments with different, FPU operand types
 	INSTEST(0xD8 0xCA, FMUL, st0, st2);
 	INSTESTMEMEQ(0xD8, 0xCA);
 
-	INSTEST(0xD9 0xE8, FLD1);
-	INSTESTMEMEQ(0xD9, 0xE8);
+	INSTEST(0xC3, RET);
+	INSTESTMEMEQ(0xC3);
+	
+	INSTEST(0xCB, RET, { FAR });
+	INSTESTMEMEQ(0xCB);
+
+	INSTEST(0xAE, SCAS, m8($rdi));
+	INSTESTMEMEQ(0xAE);
 
 	INSTEST(0x9B, FWAIT);
 	INSTESTMEMEQ(0x9B);
+
+	INSTEST(0x67 0x0F 0x01 0x02, SGDT, mem($edx));
+	INSTESTMEMEQ(0x67, 0x0F, 0x01, 0x02);
+
+	INSTEST(0x0F 0x01 0x22, SMSW, mem($rdx));
+	INSTESTMEMEQ(0x0F, 0x01, 0x22);
 }
 
 TEST("Assemble random SSE Instructions (2-3 arguments with many different prefixes)") {
-	char buf[10] = {0};
+	uint8_t buf[10] = {0};
 	
 	INSTEST(0x67 0xF3 0x0F 0x7E 0x40 0x08, MOVQ, xmm0, mem($eax, 8));
 	INSTESTMEMEQ(0x67, 0xF3, 0x0F, 0x7E, 0x40, 0x08);
@@ -106,21 +132,51 @@ TEST("Assemble random SSE Instructions (2-3 arguments with many different prefix
 	INSTEST(0x66 0x0F 0x66 0xC1, PCMPGTD, xmm0, xmm1);
 	INSTESTMEMEQ(0x66, 0x0F, 0x66, 0xC1);
 
-	// INSTEST(0xC5 0xF8 0x59 0xE6, VMULPS, xmm4, xmm0, xmm6);
-	// INSTESTMEMEQ(0xC5, 0xF8, 0x59, 0xE6);
+	INSTEST(0x66 0x44 0x0F 0x10 0x72 0x0A, MOVUPD, xmm14, mem($rdx, 10));
+	INSTESTMEMEQ(0x66, 0x44, 0x0F, 0x10, 0x72, 0x0A);
+
+	INSTEST(0x0F 0x6F 0x4A 0x0A, MOVQ, mm1, mem($rdx, 10));
+	INSTESTMEMEQ(0x0F, 0x6F, 0x4A, 0x0A);
+}
+
+TEST("Assemble AVX (VEX) Instructions (With the VEX Prefix)") {
+	uint8_t buf[13] = {0};
+
+	INSTEST(0xC5 0xF8 0x28 0xC6, VMOVAPS, xmm0, xmm6);
+	INSTESTMEMEQ(0xC5, 0xF8, 0x28, 0xC6);
+
+	INSTEST(0xC5 0xF9 0x6E 0xD0, VMOVD, xmm2, eax);
+	INSTESTMEMEQ(0xC5, 0xF9, 0x6E, 0xD0);
+	
+	INSTEST(0xC5 0xF8 0x59 0xE6, VMULPS, xmm4, xmm0, xmm6);
+	INSTESTMEMEQ(0xC5, 0xF8, 0x59, 0xE6);
+
+	// Tests VSIB
+	INSTEST(0xC4 0xE2 0xD5 0x93 0x54 0xD8 0x0A, VGATHERQPD, ymm2, mem($rax, 10, $ymm3, 8), ymm5);
+	INSTESTMEMEQ(0xC4, 0xE2, 0xD5, 0x93, 0x54, 0xD8, 0x0A);
+
+	INSTEST(0xC4 0xC1 0x58 0xC2 0x1C 0x10 0xFF, VCMPPS, xmm3, xmm4, mem($r8, 0, $rdx), imm(0xff));
+	INSTESTMEMEQ(0xC4, 0xC1, 0x58, 0xC2, 0x1C, 0x10, 0xFF);
+
+	INSTEST(0xC4 0xE3 0x75 0x4B 0x54 0x10 0x0A 0x50, VBLENDVPD, ymm2, ymm1, mem($rax, 10, $rdx), ymm5);
+	INSTESTMEMEQ(0xC4, 0xE3, 0x75, 0x4B, 0x54, 0x10, 0x0A, 0x50);
 }
 
 TEST("Stringify instructions") {
-	SUB("Stringify MOV, {rax, rax} => mov rax, rax")
-		assertstreq(x64stringify((x64) { MOV, {rax, rax}, 0 }), "\tmov\trax, rax");
+	SUB("Stringify MOV, rax, rax => mov rax, rax")
+		assertstreq(x64stringify((x64) { MOV, rax, rax }, 1), "mov rax, rax");
 
-	SUB("Stringify MOV, {rax, imm(0xABCDEF)} => mov rax, 0xABCDEF")
-		assertstreq(x64stringify((x64) { MOV, {rax, imm(0xABCDEF)}, 0 }), "\tmov\trax, 0xABCDEF");
+	SUB("Stringify MOV, rax, imm(0xABCDEF) => mov rax, 0xABCDEF")
+		assertstreq(x64stringify((x64) { MOV, rax, imm(0xABCDEF) }, 1), "mov rax, 0xABCDEF");
 
-	SUB("Stringify MOV, {rax, mem($rax, 0xABCDEF, $r8, 2, $es)} => mov rax, es:[rax + r8 * 2 + 0xABCDEF]")
-		assertstreq(x64stringify((x64) { MOV, {rax, mem($rax, 0xABCDEF, $r8, 2, $es)}, 0 }), "\tmov\trax, es:[rax + 0xABCDEF + r8 * 2]");
+	SUB("Stringify MOV, rax, mem($rax, 0xABCDEF, $r8, 2, $es) => mov rax, es:[rax + r8 * 2 + 0xABCDEF]")
+		assertstreq(x64stringify((x64) { MOV, rax, mem($rax, 0xABCDEF, $r8, 2, $es) }, 1), "mov rax, es:[rax + 0xABCDEF + r8 * 2]");
 
-	
+	SUB("Stringify LEA, rax, mem($riprel, 0xABCDEF) => lea rax, [$+11259375]")
+		assertstreq(x64stringify((x64) { LEA, rax, mem($riprel, 0xABCDEF) }, 1), "lea rax, [$+11259375]");
+
+	SUB("Stringify JMP, rel(1000) => jmp $+1000")
+		assertstreq(x64stringify((x64) { JMP, rel(1000) }, 1), "jmp $+1000");
 }
 
 #include "tests_end.h"
--- tests/bf.c
+++ tests/bf.c
@@ -4,7 +4,7 @@
 #undef IN
 #undef INT
 #undef OUT
-#include <asm/asm_x86.h>
+#include <asm/asm_x64.h>
 
 #define VEC_H_IMPLEMENTATION
 #include <vec.h>
@@ -37,69 +37,115 @@ char* prog1 = PROG(
 
 x64Ins* bf_compile(char* in) {
 	x64Ins* ret = vnew();
+	
+#ifdef _WIN32
+	x64Operand arg1 = rcx, arg2 = rdx;
+#else
+	x64Operand arg1 = rdi, arg2 = rsi;
+#endif
+	
 	vpusharr(ret, {
 		{ PUSH, rbp },
 		{ MOV, rbp, rsp },
-		{ MOV, rcx, imm(256) },
-		{ MOV, rax, im64((uint64_t)(void*)calloc)},
-		{ CALL, rax },
-		{ PUSH, rax },
+		{ MOV, rax, arg1 },
+		{ PUSH, arg1 } // RCX = argument 1. 
 	});
+
+	bool raxoverwritten = false;
 
 	while(*in) {
 		switch(*in) {
-		case '>': vpush(ret, { DEC, m64($rbp, -8) }); break;
-		case '<': vpush(ret, { INC, m64($rbp, -8) }); break;
-		case '+': vpusharr(ret, { { MOV, rax, m64($rbp, -8) }, { INC, m8($rax) } }); break;
-		case '-': vpusharr(ret, { { MOV, rax, m64($rbp, -8) }, { DEC, m8($rax) } }); break;
-		case '[': vpusharr(ret, { { LEA, rax, m64($riprel, -1) }, { PUSH, rax } }); break;
-		case '.':
+		case '>':
+		case '<':
+			vpush(ret, { *in == '>' ? INC : DEC, m64($rbp, -8) });
+			
+			if(!raxoverwritten)
+				vpush(ret, { *in == '>' ? INC : DEC, rax });
+			break;
+		case '+':
+		case '-': {
+			if(raxoverwritten) {
+				vpush(ret, { MOV, rax, m64($rbp, -8) });
+				raxoverwritten = false;
+			}
+
+			vpush(ret, { *in == '+' ? INC : DEC, m8($rax) });
+			break;
+		}
+		case '[':
 			vpusharr(ret, {
+				{ LEA, rsi, m64($riprel, 0) }, // 0 here means $+0 or the current instruction
+				{ PUSH, rsi }
+			});
+			raxoverwritten = true; // Because ] overwrites rax, so it's probably overwritten and if it's not, nothing bad happens.
+			break;
+		case '.':
+			raxoverwritten = true;
+			vpusharr(ret, {
+				{ TEST, arg2, arg2 }, // If there's no putchar function passed in, just skip.
+				{ JZ, rel(6) },
 				{ MOV, rax, mem($rbp, -8) },
 				{ MOV, rcx, mem($rax) },
-				{ SUB, rsp, imm(64) },
-				{ MOV, rax, im64((uint64_t)(void*)putchar) },
+				{ SUB, rsp, imm(64) }, // Should investigate aligining the stack to 16 bytes but this works for now(what msvc does).
+				{ MOV, rax, arg2 },
 				{ CALL, rax },
 				{ ADD, rsp, imm(64) },
 			});
 			break;
 		case ']':
+			raxoverwritten = true;
 			vpusharr(ret, {
 				{ MOV, rax, mem($rbp, -8) },
 				{ CMP, m8($rax), imm(0) },
-				{ JZ, rel(2) },
+				{ JZ, rel(3) },
 				{ POP, rax },
 				{ JMP, rax },
 			});
-			break;
 		default: break;
 		}
-		in ++;
+		in++;
 	}
 
 	vpusharr(ret, {
 		{ MOV, rsp, rbp },
 		{ POP, rbp },
 		{ RET },
-		{ 0 }
 	});
 	return ret;
 }
 
-TEST("Print a letter") {
-	x64Ins* ins = bf_compile("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
-	uint32_t len;
-	void* program = x64as(vlen(ins), ins, &len);
-	void (*hi)() = run(program, len);
-	hi();
+
+char buf[200] = {};
+void custom_putchar(int c) {
+	buf[strlen(buf)] = c;
 }
 
-TEST("Print hello world") {
-	x64Ins* ins = bf_compile(prog1);
+void (*hi)(char* buf, void* printfn);
+TEST("Print a letter: Compile") {
+	x64Ins* ins = bf_compile("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.");
 	uint32_t len;
-	void* program = x64as(vlen(ins), ins, &len);
-	void (*hi)() = run(program, len);
-	hi();
+	void* compiled = x64as(ins, vlen(ins), &len);
+	hi = (void*) x64_exec(compiled, len);
+}
+
+TEST("Print a letter: Run") {
+	char buf[2] = {};
+	hi(buf);
+	expectstreq(buf, "s");
+}
+
+
+TEST("Print hello world: Compile") {
+	x64Ins* ins = bf_compile(prog1);
+	uint32_t len = 0;
+	void* compiled = x64as(ins, vlen(ins), &len);
+	hi = (void*) x64_exec(compiled, len);
+}
+
+TEST("Print hello world: Run") {
+	char buf[20] = {0};
+	hi(buf);
+	expectstreq(buf, "Hello World!");
 }
 
 #include "tests_end.h"
